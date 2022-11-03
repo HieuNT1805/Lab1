3bcd13330d5fde2eb6dc472cdfcb3382f9f2eaa3
HieuNguyen <91725595+HieuNT1805@users.noreply.github.com>
2022-11-03 15:03:08 +0700

    ex6

--- ex1/src/App.js
+++ ex1/src/App.js
@@ -1,14 +1,72 @@
-import * as React from "react"
-import {FadeLoader} from "react-spinners";
-import MyPage from "./MyPage";
+import "typeface-roboto";
+import React from "react";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
 
-function App() {
-  
-  return (
-   <React.Suspense fallback={<FadeLoader color={"lightblue"} size={150}/>}>
-      <MyPage/>
-   </React.Suspense>
+const headFooterStyle = {
+  padding:8,
+  textAlign:"center",
+};
+const mainStyle = {
+  padding:16,
+  textAlign:"center"
+}
+const navStyle = {marginLeft: 5};
+
+export default function App(){
+  return(
+    <div style={{flexGrow:1}}>
+      <Grid container spacing={3}>
+        <Paper style={headFooterStyle}>
+          <Typography>Header</Typography>
+        </Paper>
+      </Grid>
+      <Grid item xs={4}>
+        <Paper>
+          <Grid container spacing={2} direction="column">
+            <Grid item xs={12}>
+              <Typography style={navStyle}>Nav Item 1</Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Grid item xs={12}>
+                <Typography style={navStyle}>Nav Item 2</Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography style={navStyle}>Nav Item 3</Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography style={navStyle}>Nav Item 4</Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Paper>
+      </Grid>
+      <Grid item xs={8}>
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <Paper style={mainStyle}>
+              <Typography>Main content 2</Typography>
+            </Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <Paper style={mainStyle}>
+              <Typography>Main content 3</Typography>
+            </Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <Paper style={mainStyle}>
+              <Typography>Main content 4</Typography>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item xs={12}>
+        <Paper style={mainStyle}>
+          <Typography>Footer</Typography>
+        </Paper>
+      </Grid>
+
+    </div>
   )
 }
-
-export default App;
